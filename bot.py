33f62c353a08b1acd0a195bb29da335b9a1c82ae
Ryan Vall <rav43@case.edu>
2016-10-02 22:52:17 -0400

    added username/pw files locally, added gitignore

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@
 
 import praw
 
+username = open("username.txt").read().rstrip()
+password = open("password.txt").read().rstrip()
 user_agent = ("Overwatch Info Fetcher by /u/Verinix v1.0")
 
-r = praw.Reddit(user_agent = user_agent)
+reddit = praw.Reddit(user_agent = user_agent)
+reddit.login(username = username, password = password)
 
 subreddit = r.get_subreddit("overwatch")
