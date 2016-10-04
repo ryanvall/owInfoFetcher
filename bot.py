1dce233461dd3da7c4b894dd6b06f331a1324a27
Ryan Vall <rav43@case.edu>
2016-10-03 23:25:09 -0400

    added test reply function, get from unofficial api, basic functionality working

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 
 import praw
 import re
+import requests
+import time
 
 username = open("username.txt").read().rstrip()
 password = open("password.txt").read().rstrip()
@@ -11,22 +13,35 @@ bot_tail = "\n\nBeep boop, I am a bot. Message /u/Verinix for feedback or other 
 reddit = praw.Reddit(user_agent = user_agent)
 reddit.login(username = username, password = password)
 
-subreddit = reddit.get_subreddit("overwatch")
+subreddit = reddit.get_subreddit("test")
 already_done = set()
-hero_pattern = re.compile("\[(?P<hero>.*?)/]")
+hero_pattern = re.compile("\[\[(?P<hero>.*?)\]\]")
+
+def return_info(hero):
+  result = ""
+  r = requests.get('https://api.lootbox.eu/patch_notes')
+  if r.status_code == 200:
+    json = r.json()
+    result = json["patchNotes"][0]["detail"]
+  else:
+    result = "Error contacting Lootbox API."
+
+  return result
 
 running = True
 while running:
   for comment in praw.helpers.flatten_tree(subreddit.get_comments()):
-    match = call_pattern.search(comment.body)
-    if not match:
+    if comment not in already_done:
+      already_done.add(comment)
+      match = hero_pattern.search(comment.body)
+      if not match:
+        continue
+      hero = match.group("hero")
+      reply_body = return_info(hero)
+      comment.reply(hero + "\n" + reply_body + "\n" + bot_tail)
+    else:
       continue
-    hero = match.group("hero")
-    reply_body = return_info(hero)
-    comment.reply(reply_body + bot_tail)
 
   # sleep for a minute
   time.sleep(60)
 
-def return_info(hero)
-  return "todo"
