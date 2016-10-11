6434462ddb6c64a13de0f5368c7e0f0587b5f447
Ryan Vall <rav43@case.edu>
2016-10-11 11:06:09 -0400

    added utils file with custom exception, separated patch notes and hero methods

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,7 @@ import praw
 import re
 import requests
 import time
+import utils
 
 username = open("username.txt").read().rstrip()
 password = open("password.txt").read().rstrip()
@@ -14,25 +15,45 @@ reddit = praw.Reddit(user_agent = user_agent)
 reddit.login(username = username, password = password)
 
 subreddit = reddit.get_subreddit("test")
-already_done = set()
+already_done_comments = set()
+already_done_submissions = set()
 hero_pattern = re.compile("\[\[(?P<hero>.*?)\]\]")
 
 def return_info(hero):
-  result = ""
   r = requests.get('https://api.lootbox.eu/patch_notes')
   if r.status_code == 200:
     json = r.json()
     result = json["patchNotes"][0]["detail"]
   else:
-    result = "Error contacting Lootbox API."
+    raise utils.APIConnectionException("Error contacting Lootbox API, error code %d." % r.status_code)
+    result = "Error"
+  return result
 
+def get_patch_notes():
+  r = requests.get('https://api.lootbox.eu/patch_notes')
+  if r.status_code == 200:
+    json = r.json()
+    result = json["patchNotes"][0]["detail"]
+  else:
+    raise utils.APIConnectionException("Error contacting Lootbox API.")
+    result = "Error contacting Lootbox API."
   return result
 
 running = True
 while running:
+  # check threads for patch notes
+  for submission in subreddit.get_new(limit = SUBMISSION_LIMIT):
+    if submission not in already_done_submissions:
+      already_done_submissions.add(submission)
+      reply_body = get_patch_notes()
+      reply = reply_body + "\n" + bot_tail
+      submission.add_comment(reply)
+    else:
+      continue
+  # check comments for calls about heros/etc
   for comment in praw.helpers.flatten_tree(subreddit.get_comments()):
-    if comment not in already_done:
-      already_done.add(comment)
+    if comment not in already_done_comments:
+      already_done_comments.add(comment)
       match = hero_pattern.search(comment.body)
       if not match:
         continue
@@ -44,4 +65,3 @@ while running:
 
   # sleep for a minute
   time.sleep(60)
-
